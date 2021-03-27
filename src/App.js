8eb39e5020b9833c357ebb7492fbe82ca20840cd
sujoyduttajad <sujoyduttajad@gmail.com>
2021-03-27 16:41:31 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Coin from './components/Coin'
+
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => (
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  ))
+  console.log(filteredCoins);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className="coin-text">Crypto Price Tracker</h1>
+        <form>
+          <input 
+            type="text" 
+            placeholder="Search a currency"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {
+        filteredCoins.map(coin => {
+          return (
+            <Coin 
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+            />
+          )
+        })
+      }
     </div>
   );
 }
