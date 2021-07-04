8852db7ee4efd7ac2507d177beb4102eca3ff658
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-04 16:28:30 +0530

    added new icon and changed header

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -11,12 +11,24 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap}) => 
                     <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <div className="container-1"><p className="coin-price"><span>Price: </span><p>${price}</p></p></div>
-                    <div className="container-2"><p className="coin-volume">${volume.toLocaleString()}</p></div>
+                    <div className="container-1">
+                        <p className="coin-price">
+                            <span>Price: </span><p>${price}</p>
+                        </p>
+                    </div>
+                    <div className="container-2">
+                        <p className="coin-volume">
+                            <span>Volume: </span><p>${volume.toLocaleString()}</p>
+                        </p>
+                    </div>
                     <div className="container-3">{priceChange < 0 ? (
-                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                        <p className="coin-percent red">
+                            <span>Coin-Percent: </span><p>{priceChange.toFixed(2)}%</p>
+                        </p>
                     ) : (
-                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                        <p className="coin-percent green">
+                            <span>Coin-Percent: </span><p>{priceChange.toFixed(2)}%</p>
+                        </p>
                     )}</div>
                     <div className="container-4">
                         <p className="coin-marketcap">
