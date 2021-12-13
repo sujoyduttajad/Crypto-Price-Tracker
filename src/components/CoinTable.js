34ca96d1bec41861feb0735a6121c29e7c69aa80
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-13 19:49:24 +0530

    Update CoinTable.js

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -22,31 +22,7 @@ import Switch from '@mui/material/Switch';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
 
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -83,31 +59,31 @@ const headCells = [
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Token Name',
   },
   {
-    id: 'calories',
+    id: 'price',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
+    label: 'Price',
   },
   {
-    id: 'fat',
+    id: 'volume',
     numeric: true,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Volume',
   },
   {
-    id: 'carbs',
+    id: 'coinPercent',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'Coin Percent',
   },
   {
-    id: 'protein',
+    id: 'mktCap',
     numeric: true,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Market Cap',
   },
 ];
 
@@ -197,7 +173,7 @@ const EnhancedTableToolbar = (props) => {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Market Report
         </Typography>
       )}
         <Tooltip title="Filter list">
@@ -216,11 +192,34 @@ EnhancedTableToolbar.propTypes = {
 
 export default function CoinTable({ filteredCoins }) {
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('price');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  function createData(name, price, volume, coinPercent, mktCap) {
+    return {
+      name,
+      price,
+      volume,
+      coinPercent,
+      mktCap,
+    };
+  }
+  
+  const rows = 
+    filteredCoins.map((coin) => {
+      createData(
+        coin.name, 
+        coin.current_price, 
+        coin.total_volume, 
+        coin.price_change_percentage_24h,
+        coin.market_cap
+        )
+    });
+    // createData('Oreo', 437, 18.0, 63, 4.0),
+  
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -330,10 +329,10 @@ export default function CoinTable({ filteredCoins }) {
                       >
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.price}</TableCell>
+                      <TableCell align="right">{row.volume}</TableCell>
+                      <TableCell align="right">{row.coinPercent}</TableCell>
+                      <TableCell align="right">{row.mktCap}</TableCell>
                     </TableRow>
                   );
                 })}
