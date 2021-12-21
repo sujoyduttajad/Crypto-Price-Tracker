9ca0f6dd03f78efe7164d9cec6908c6bf9af69cd
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-21 22:19:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import './App.css';
+import './styles/global.scss';
 import CoinTable from './components/CoinTable'
 import Navbar from './components/Navbar'
 
