d743284b8d8ab71508416f3cafa67f83b515e660
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-21 21:00:51 +0530

    Added new pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,15 @@ const Navbar = ({
             <div className='coin-logo-container'>
                 <img className='coin-logo' src={Logo} alt="logo" />
             </div>
+            <div className='coin-pages-container'>
+                <h3>Market</h3>
+            </div>
+            <div className='coin-pages-container'>
+                <h3>Exchange</h3>
+            </div>
+            <div className='coin-pages-container'>
+                <h3>Categories</h3>
+            </div>
             <form className='coin-input-container'>
                 <input 
                     type="text" 
