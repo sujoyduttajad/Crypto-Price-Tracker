5e652a3d709f64b269a276fc84e32ceb7ab446a6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-22 13:54:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   const filteredCoins = coins.filter(coin => (
     coin.name.toLowerCase().includes(search.toLowerCase())
   ))
-  console.log(filteredCoins);
+  // console.log(filteredCoins);
 
   return (
     <div className="coin-app">
