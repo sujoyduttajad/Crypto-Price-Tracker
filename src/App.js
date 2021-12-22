b98a4adddda33e62e808f4069c39f0902c6dd33a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-22 15:16:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,14 @@ function App() {
       console.log(res.data);
     })
     .catch(error => console.log(error))
-  }, [])
+  }, []);
+
+  // const getCryptoCoinData = async () => {
+  //   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'); 
+  //   const coinData = await response.json();
+  //   setCoins(coinData.data);
+  //   console.log(coins);
+  // }
 
   const handleChange = e => {
     setSearch(e.target.value)
