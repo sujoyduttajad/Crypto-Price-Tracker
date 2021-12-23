a84f1ca023df731d2f1c13a0e913c094548e30fb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-23 20:40:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ function App() {
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data);
-      console.log(res.data);
+      setLoading(false);
     })
     .catch(error => console.log(error))
   }, []);
@@ -30,6 +31,7 @@ function App() {
     coin.name.toLowerCase().includes(search.toLowerCase())
   ))
   // console.log(filteredCoins);
+  console.log(loading);
 
   return (
           <div className="coin-app">
