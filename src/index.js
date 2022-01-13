5920f784f3c3b96327d8953bf69a5767d376b982
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-13 19:02:17 +0530

    Added Provider wrapper

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './features/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store} >
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
