352633b9aa1ab20e7cc88f97ee4ec63f7001bb10
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 05:15:04 +0530

    Update Exchange.js

---
+++ src/pages/Exchange.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Exchange = ({ handleChange }) => {
+  return (
+    <>
+      <Navbar handleChange={handleChange} />
+      <div>Exchange</div>
+    </>
+  );
+};
+
+export default Exchange;
