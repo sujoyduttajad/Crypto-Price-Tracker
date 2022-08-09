9f5841a0e1b41c2685bf6bddb301fc1733f304a1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 05:03:10 +0530

    Update CoinTable.js

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -1,135 +1,136 @@
-import React, {useState, useEffect} from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import { makeStyles } from '@mui/styles';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
-import { useDispatch } from 'react-redux';
-import { update } from '../features/eachCoinSlice'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
+import { makeStyles } from "@mui/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import { useDispatch } from "react-redux";
+import { update } from "../features/eachCoinSlice";
 
 const useStyles = makeStyles({
   tableContent: {
-    fontFamily: 'Montserrat',
+    fontFamily: "Montserrat",
     // color: (props) => props.color,
   },
   tableRows: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
     margin: 0,
   },
   avatar: {
-    colorDefault: 'purple'
-  }
+    colorDefault: "purple",
+  },
 });
 
 const theme = createTheme({
-  palette: { mode: 'dark' },
+  palette: { mode: "dark" },
   components: {
     MuiTypography: {
       styleOverrides: {
         root: {
-          fontFamily: 'Montserrat',
+          fontFamily: "Montserrat",
           fontWeight: 600,
-          fontSize: '2.1em',
-          color: '#eee'
+          fontSize: "2.1em",
+          color: "#eee",
         },
       },
     },
     MuiTableCell: {
       styleOverrides: {
         root: {
-          fontFamily: 'Montserrat',
+          fontFamily: "Montserrat",
           fontWeight: 400,
-          cursor: 'pointer',
+          cursor: "pointer",
         },
       },
     },
     MuiTableSortLabel: {
       styleOverrides: {
         root: {
-          fontFamily: 'Montserrat',
+          fontFamily: "Montserrat",
           fontWeight: 500,
-          fontSize: '1.2em'
+          fontSize: "1.2em",
         },
       },
     },
     MuiTablePagination: {
       styleOverrides: {
         root: {
-          fontFamily: 'Montserrat',
+          fontFamily: "Montserrat",
           fontWeight: 700,
-          color: '#fff'
-        },
-    }},
-    MuiPaper: { 
-      styleOverrides: {
-        root: {
-          border: '0.1px solid #333333',
+          color: "#fff",
         },
       },
-    }
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          border: "0.1px solid #333333",
+        },
+      },
+    },
   },
 });
 
 const headCells = [
   {
-    id: 'rank',
+    id: "rank",
     numeric: true,
     disablePadding: true,
-    label: '#Rank',
+    label: "#Rank",
   },
   {
-    id: 'image',
+    id: "image",
     numeric: false,
     disablePadding: true,
-    label: '',
+    label: "",
   },
   {
-    id: 'name',
+    id: "name",
     numeric: false,
     disablePadding: true,
-    label: 'Token Name',
+    label: "Token Name",
   },
   {
-    id: 'price',
+    id: "price",
     numeric: true,
     disablePadding: false,
-    label: 'Price(USD)',
+    label: "Price(USD)",
   },
   {
-    id: 'volume',
+    id: "volume",
     numeric: true,
     disablePadding: false,
-    label: 'Volume(USD)',
+    label: "Volume(USD)",
   },
   {
-    id: 'coinPercent',
+    id: "coinPercent",
     numeric: true,
     disablePadding: false,
-    label: '24h Change',
+    label: "24h Change",
   },
   {
-    id: 'mktCap',
+    id: "mktCap",
     numeric: true,
     disablePadding: false,
-    label: 'Market Cap(USD)',
+    label: "Market Cap(USD)",
   },
 ];
 
@@ -139,24 +140,22 @@ function EnhancedTableHead(props) {
   return (
     <TableHead>
       <TableRow>
-        {
-          headCells.map((headCell) => (
-            <ThemeProvider theme={theme} key={headCell.id}>
-              <TableCell
-                key={headCell.id}
-                align={headCell.numeric ? 'right' : 'left'}
-                padding={headCell.disablePadding ? 'none' : 'normal'}            
-              >
-                <TableSortLabel>
-                  {headCell.label}
-                  {orderBy === headCell.id ? (
-                    <Box component="span" sx={visuallyHidden}></Box>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-            </ThemeProvider>
-          ))
-        }
+        {headCells.map((headCell) => (
+          <ThemeProvider theme={theme} key={headCell.id}>
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? "right" : "left"}
+              padding={headCell.disablePadding ? "none" : "normal"}
+            >
+              <TableSortLabel>
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}></Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          </ThemeProvider>
+        ))}
       </TableRow>
     </TableHead>
   );
@@ -179,26 +178,25 @@ const EnhancedTableToolbar = (props) => {
           pr: { xs: 1, sm: 1 },
         }}
       >
-      <Typography
-        sx={{ flex: '1 1 100%' }}
-        variant="h4"
-        id="tableTitle"
-        component="div"
-      >
-        Market Report       
-      </Typography> 
-      <IconButton style={{ marginRight: '1em'}}>     
-        <RefreshIcon 
-          fontSize='medium'
-          onClick={() => window.location.reload()} // custom refresh button 
-        />
-      </IconButton>
-      <Tooltip title="Filter list">
-        <IconButton>
-          <FilterListIcon />
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h4"
+          id="tableTitle"
+          component="div"
+        >
+          Market Report
+        </Typography>
+        <IconButton
+          style={{ marginRight: "1em" }}
+          onClick={() => window.location.reload()} // custom refresh button
+        >
+          <RefreshIcon fontSize="medium" />
         </IconButton>
-      </Tooltip>       
-      
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </ThemeProvider>
   );
@@ -218,7 +216,7 @@ export default function CoinTable({ filteredCoins }) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  const [coinId, setCoinId] = useState('');
+  const [coinId, setCoinId] = useState("");
   const dispatch = useDispatch();
 
   function createData(name, price, volume, coinPercent, mktCap) {
@@ -231,15 +229,15 @@ export default function CoinTable({ filteredCoins }) {
     };
   }
   const rows = filteredCoins;
-    rows.map((coin) => {
-      return createData(
-        coin.name, 
-        coin.current_price, 
-        coin.total_volume, 
-        coin.price_change_percentage_24h,
-        coin.market_cap
-        )
-    });
+  rows.map((coin) => {
+    return createData(
+      coin.name,
+      coin.current_price,
+      coin.total_volume,
+      coin.price_change_percentage_24h,
+      coin.market_cap
+    );
+  });
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
@@ -249,17 +247,17 @@ export default function CoinTable({ filteredCoins }) {
     }
     setSelected([]);
   };
-  
-  const handleClick =  async (e, id) => {
-    // await setCoinId(id);
-    await dispatch(update({coinId}))
-   };
 
-   console.log(coinId);
-   
-   useEffect(() => {
+  const handleClick = async (e, id) => {
+    // await setCoinId(id);
+    await dispatch(update({ coinId }));
+  };
+
+  console.log(coinId);
+
+  useEffect(() => {
     return () => handleClick();
-  }, [])
+  }, []);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -278,121 +276,139 @@ export default function CoinTable({ filteredCoins }) {
 
   return (
     <div className="crypto-container">
-    <ThemeProvider theme={theme}>
-      <Paper 
-        sx={{ width: '100%', mb: 2 }}
-        elevation={1}
-      >
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            stickyHeader 
-            aria-label="sticky table"
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={'medium'}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              onSelectAllClick={handleSelectAllClick}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {
-                rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  
-                  const labelId = `enhanced-table-checkbox-${index}`;
+      <ThemeProvider theme={theme}>
+        <Paper sx={{ width: "100%", mb: 2 }} elevation={1}>
+          <EnhancedTableToolbar numSelected={selected.length} />
+          <TableContainer>
+            <Table
+              stickyHeader
+              aria-label="sticky table"
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size={"medium"}
+            >
+              <EnhancedTableHead
+                numSelected={selected.length}
+                onSelectAllClick={handleSelectAllClick}
+                rowCount={rows.length}
+              />
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) => {
+                    const labelId = `enhanced-table-checkbox-${index}`;
 
-                  return (
-                    <ThemeProvider theme={theme} key={row.id}>
-                    <TableRow
-                      hover
-                      onClick={(e) => handleClick(e, row.id)}                    
-                      tabIndex={-1}
-                      key={row.id}
-                      value={row.id}
-                      className={classes.tableContent}
-                    >
-                      <TableCell padding="checkbox">
-                        <Avatar 
-                          className={classes.avatar} 
-                          sx={{ width: 30, height: 30 }}
-                          // variant='rounded'
-                          style={{ colorDefault: '#000'}}
+                    return (
+                      <ThemeProvider theme={theme} key={row.id}>
+                        <TableRow
+                          hover
+                          onClick={(e) => handleClick(e, row.id)}
+                          tabIndex={-1}
+                          key={row.id}
+                          value={row.id}
+                          className={classes.tableContent}
                         >
-                          <p style={{ 
-                            fontSize: '15px', 
-                            fontWeight: 500,
-                            color: '#fff'
-                            }}
+                          <TableCell padding="checkbox">
+                            <Avatar
+                              className={classes.avatar}
+                              sx={{ width: 30, height: 30 }}
+                              // variant='rounded'
+                              style={{ colorDefault: "#000" }}
+                            >
+                              <p
+                                style={{
+                                  fontSize: "15px",
+                                  fontWeight: 500,
+                                  color: "#fff",
+                                }}
+                              >
+                                {row.market_cap_rank}
+                              </p>
+                            </Avatar>
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding="none"
+                            align="right"
                           >
-                            {row.market_cap_rank}
-                          </p>
-                        </Avatar>
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                        align='right'
-                      >
-                        <img className='coin__image' src={row.image} alt={row.name} />
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                        className={classes.tableRows}
-                      >
-                        {row.name}                      
-                      </TableCell>
-                      <TableCell align="right" className={classes.tableRows}>
-                        ${row.current_price}
-                      </TableCell>
-                      <TableCell align="right" className={classes.tableRows}>
-                        ${row.total_volume}
-                      </TableCell>
-                      <TableCell 
-                        align="right" 
-                        className={classes.tableRows}                       
-                      >
-                        <span 
-                          style={{
-                            color: `${row.price_change_percentage_24h > 0 ? '#1dd15a' : '#ef5959'}`,
-                            backgroundColor: `${row.price_change_percentage_24h > 0 ? 'rgba(29, 209, 90, 0.2)' : 'rgba(239, 89, 89, 0.2)'}`,
-                            padding: '5px',
-                            borderRadius: '3.5px',
-                          }}
-                        >
-                          {row.price_change_percentage_24h}
-                        </span>
-                      </TableCell>
-                      <TableCell align="right" className={classes.tableRows}>
-                        ${row.market_cap}
-                      </TableCell>
-                    </TableRow>
-                    </ThemeProvider>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <ThemeProvider theme={theme}>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 50]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </ThemeProvider>
-      </Paper>
-    </ThemeProvider>
+                            <img
+                              className="coin__image"
+                              src={row.image}
+                              alt={row.name}
+                            />
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding="none"
+                            className={classes.tableRows}
+                          >
+                            {row.name}
+                          </TableCell>
+                          <TableCell
+                            align="right"
+                            className={classes.tableRows}
+                          >
+                            ${row.current_price}
+                          </TableCell>
+                          <TableCell
+                            align="right"
+                            className={classes.tableRows}
+                          >
+                            ${row.total_volume}
+                          </TableCell>
+                          <TableCell
+                            align="right"
+                            className={classes.tableRows}
+                          >
+                            <span
+                              style={{
+                                color: `${
+                                  row.price_change_percentage_24h > 0
+                                    ? "#1dd15a"
+                                    : "#ef5959"
+                                }`,
+                                backgroundColor: `${
+                                  row.price_change_percentage_24h > 0
+                                    ? "rgba(29, 209, 90, 0.2)"
+                                    : "rgba(239, 89, 89, 0.2)"
+                                }`,
+                                padding: "5px",
+                                borderRadius: "3.5px",
+                              }}
+                            >
+                              {row.price_change_percentage_24h}
+                            </span>
+                          </TableCell>
+                          <TableCell
+                            align="right"
+                            className={classes.tableRows}
+                          >
+                            ${row.market_cap}
+                          </TableCell>
+                        </TableRow>
+                      </ThemeProvider>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <ThemeProvider theme={theme}>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </ThemeProvider>
+        </Paper>
+      </ThemeProvider>
     </div>
   );
 }
