ccd5b9b690ef4f48dc2c96856e3e6bbb5d0c3fbb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 06:12:25 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Logo from "../images/coinize2.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = ({ handleChange }) => {
   let activeClassName = "nav-active";
+  const location = useLocation();
+  console.log(location.pathname);
+  const isActive = location.pathname;
 
   return (
     <nav className="coin-search">
@@ -15,25 +18,37 @@ const Navbar = ({ handleChange }) => {
         </NavLink>
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? activeClassName : undefined)}
+          //   className={isActive === '/' ? activeClassName : undefined}
         >
-          <div className="coin-pages-container">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/" ? activeClassName : undefined
+            }`}
+          >
             <h3>Market</h3>
           </div>
         </NavLink>
         <NavLink
           to="/exchange"
-          className={({ isActive }) => (isActive ? activeClassName : undefined)}
+          //   className={isActive === "/exchange" ? activeClassName : undefined}
         >
-          <div className="coin-pages-container">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/exchange" ? activeClassName : undefined
+            }`}
+          >
             <h3>Exchange</h3>
           </div>
         </NavLink>
         <NavLink
           to="/categories"
-          className={({ isActive }) => (isActive ? activeClassName : undefined)}
+          //   className={isActive === "/categories" ? activeClassName : undefined}
         >
-          <div className="coin-pages-container">
+          <div
+            className={`coin-pages-container ${
+              isActive === "/categories" ? activeClassName : undefined
+            }`}
+          >
             <h3>Categories</h3>
           </div>
         </NavLink>
