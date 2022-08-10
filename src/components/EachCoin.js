e97183649f6be5ebeda459c800436ceecc5c816a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 09:51:18 +0530

    Update EachCoin.js

--- src/components/EachCoin.js
+++ src/components/EachCoin.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const EachCoin = ({coin}) => {
+const EachCoin = ({ coin }) => {
+  const coinState = useSelector((state) => state.eachCoin);
+  console.log(coinState);
+  return (
+    <div>
+      <h1>Cpoiny</h1>
+    </div>
+  );
+};
 
-console.log(coin)
-    return (
-        <div>
-            <h1>{coin.symbol}</h1>
-        </div>
-    )
-}
-
-export default EachCoin
+export default EachCoin;
