d80bc5ccbee3f0ab958dbae5a047322cb8437fdb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 12:07:40 +0530

    Update EachCoin.js

--- src/components/EachCoin.js
+++ src/components/EachCoin.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { update, add } from "../features/eachCoinSlice";
 
-const EachCoin = ({ coin }) => {
+const EachCoin = ({ setLoading }) => {
+  const dispatch = useDispatch();
   const coinState = useSelector((state) => state.eachCoin);
   console.log(coinState);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.coingecko.com/api/v3/coins/${coinState}`)
+      .then((res) => {
+        dispatch(add({ coin: res.data }));
+        setLoading(false);
+        // console.log(res);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
       <h1>Cpoiny</h1>
