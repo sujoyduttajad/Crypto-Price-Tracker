fba42448c26f4d660c8399bda6d6de1d0909c160
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-10 05:42:49 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,54 @@
-import React from 'react'
-import Logo from '../images/coinize2.svg'
-import { Link } from "react-router-dom"
+import React from "react";
+import Logo from "../images/coinize2.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = ({ handleChange }) => {
-    return (
-        <nav className='coin-search'>
-            <div className='coin-logo-container'>
-                <Link to="/">
-                    <div className='coin-image-container'>
-                        <img className='coin-logo' src={Logo} alt="logo" />
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className='coin-pages-container'>
-                        <h3>Market</h3>
-                    </div>
-                </Link>
-                <Link to="/exchange">
-                    <div className='coin-pages-container'>
-                        <h3>Exchange</h3>
-                    </div>
-                </Link>
-                <Link to="/categories">
-                    <div className='coin-pages-container'>
-                        <h3>Categories</h3>
-                    </div>
-                </Link>
-            </div>
-            
-            <form className='coin-input-container'>
-                <input 
-                    type="text" 
-                    placeholder="Search a currency"
-                    className="coin-input"
-                    onChange={handleChange}
-                />
-            </form>
-      </nav>
-    )
-}
+  let activeClassName = "active";
 
-export default Navbar
+  return (
+    <nav className="coin-search">
+      <div className="coin-logo-container">
+        <NavLink to="/">
+          <div className="coin-image-container">
+            <img className="coin-logo" src={Logo} alt="logo" />
+          </div>
+        </NavLink>
+        <NavLink 
+            to="/"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+        >
+          <div
+            className={`coin-pages-container ${({isActive}) => 
+              isActive ? activeClassName : undefined
+            }`}
+          >
+            <h3>Market</h3>
+          </div>
+        </NavLink>
+        <NavLink to="/exchange">
+          <div className="coin-pages-container">
+            <h3>Exchange</h3>
+          </div>
+        </NavLink>
+        <NavLink to="/categories">
+          <div className="coin-pages-container">
+            <h3>Categories</h3>
+          </div>
+        </NavLink>
+      </div>
+
+      <form className="coin-input-container">
+        <input
+          type="text"
+          placeholder="Search a currency"
+          className="coin-input"
+          onChange={handleChange}
+        />
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
