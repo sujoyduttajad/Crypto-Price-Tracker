3d0567b6c2c1c721c1f31a7504a7d485ae3197ec
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 14:57:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./styles/global.scss";
 import CoinTable from "./pages/CoinTable";
 import { Routes, Route } from "react-router-dom";
-import EachCoin from "./components/EachCoin";
+import EachCoin from "./pages/EachCoin";
 import { useSelector } from "react-redux";
 import Exchange from "./pages/Exchange";
 import CoinCategories from "./pages/CoinCategories";
