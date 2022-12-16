61e4614bddeae0bf2a15923567439f062ee3f620
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 12:29:18 +0530

    Update TableHead.js

---
+++ src/components/TableHead.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Box, TableCell, TableHead, TableRow, TableSortLabel, ThemeProvider } from '@mui/material';
+import { visuallyHidden } from '@mui/utils';
+import { headCells } from '../utils/data';
+import PropTypes from "prop-types";
+import { theme } from '../materialUi/theme';
+
+export default function EnhancedTableHead(props) {
+    const { orderBy } = props;
+  
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <ThemeProvider theme={theme} key={headCell.id}>
+              <TableCell
+                key={headCell.id}
+                align={headCell.numeric ? "right" : "left"}
+                padding={headCell.disablePadding ? "none" : "normal"}
+              >
+                <TableSortLabel>
+                  {headCell.label}
+                  {orderBy === headCell.id ? (
+                    <Box component="span" sx={visuallyHidden}></Box>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
+            </ThemeProvider>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+  
+  EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
+
