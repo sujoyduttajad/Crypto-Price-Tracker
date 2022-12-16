d2c99c97b5f4dcb52402499ba676ebe631593db5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 13:15:20 +0530

    Update EnhancedTableHead.js

--- src/components/EnhancedTableHead.js
+++ src/components/EnhancedTableHead.js
@@ -11,12 +11,14 @@ import { visuallyHidden } from "@mui/utils";
 import { headCells } from "../utils/data";
 import PropTypes from "prop-types";
 import { theme } from "../materialUi/theme";
+import { useStyles } from "../materialUi/GlobalStyles";
 
 export default function EnhancedTableHead(props) {
   const { orderBy } = props;
+  const classes = useStyles();
 
   return (
-    <TableHead>
+    <TableHead className={classes.tablehHead}>
       <TableRow>
         {headCells.map((headCell) => (
           <ThemeProvider theme={theme} key={headCell.id}>
