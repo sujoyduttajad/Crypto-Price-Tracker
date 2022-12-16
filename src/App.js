3ebd39d4fa4be44b7e9d82379c024f8f185392a9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 15:45:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
     ).then((res) => {
       res.json();
+      setCoins(data);
       setLoading(false);
     })
   );
@@ -53,34 +54,32 @@ function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="coin-app">
-        <Routes>
-          <Route
-            path="/"
-            exact
-            element={
-              <CoinTable
-                filteredCoins={filteredCoins}
-                handleChange={handleChange}
-                setLoading={setLoading}
-              />
-            }
-          />
-          <Route
-            path="/exchange"
-            exact
-            element={<Exchange handleChange={handleChange} />}
-          />
-          <Route
-            path="/categories"
-            exact
-            element={<CoinCategories handleChange={handleChange} />}
-          />
-          <Route path="/:ID" element={<EachCoin setLoading={setLoading} />} />
-        </Routes>
-      </div>
-    </QueryClientProvider>
+    <Routes>
+      <QueryClientProvider client={queryClient}>
+        <Route
+          path="/"
+          exact
+          element={
+            <CoinTable
+              filteredCoins={filteredCoins}
+              handleChange={handleChange}
+              setLoading={setLoading}
+            />
+          }
+        />
+        <Route
+          path="/exchange"
+          exact
+          element={<Exchange handleChange={handleChange} />}
+        />
+        <Route
+          path="/categories"
+          exact
+          element={<CoinCategories handleChange={handleChange} />}
+        />
+        <Route path="/:ID" element={<EachCoin setLoading={setLoading} />} />
+      </QueryClientProvider>
+    </Routes>
   );
 }
 
