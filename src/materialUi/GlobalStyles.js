db78a5dd90c1c74bdd59b82588fbce2e35f97e70
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 12:42:52 +0530

    Create GlobalStyles.js

---
+++ src/materialUi/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles({
+  tableContent: {
+    fontFamily: "Montserrat",
+    // color: (props) => props.color,
+  },
+  tableRows: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    margin: 0,
+  },
+  avatar: {
+    colorDefault: "purple",
+  },
+});
