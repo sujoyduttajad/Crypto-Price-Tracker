350ff9b2ab6e6889d70336cb26d8c25732af05d1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 12:23:26 +0530

    Create data.js

---
+++ src/utils/data.js
@@ -0,0 +1,45 @@
+
+export const headCells = [
+    {
+      id: "rank",
+      numeric: true,
+      disablePadding: true,
+      label: "#Rank",
+    },
+    {
+      id: "image",
+      numeric: false,
+      disablePadding: true,
+      label: "",
+    },
+    {
+      id: "name",
+      numeric: false,
+      disablePadding: true,
+      label: "Token Name",
+    },
+    {
+      id: "price",
+      numeric: true,
+      disablePadding: false,
+      label: "Price(USD)",
+    },
+    {
+      id: "volume",
+      numeric: true,
+      disablePadding: false,
+      label: "Volume(USD)",
+    },
+    {
+      id: "coinPercent",
+      numeric: true,
+      disablePadding: false,
+      label: "24h Change",
+    },
+    {
+      id: "mktCap",
+      numeric: true,
+      disablePadding: false,
+      label: "Market Cap(USD)",
+    },
+  ];
