96d82c0555ebe760f1fdab5774a26a7e124545ed
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 14:55:48 +0530

    Create EachCoin.js

---
+++ src/pages/EachCoin.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { update, add } from "../features/eachCoinSlice";
+import { useQuery } from "react-query";
+
+const EachCoin = ({ setLoading, coinId }) => {
+  const dispatch = useDispatch();
+  const coinState = useSelector((state) => state.eachCoin);
+  console.log(coinState);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://api.coingecko.com/api/v3/coins/${coinState}`)
+  //     .then((res) => {
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
+
+  const { data, error } = useQuery("repoData", () =>
+    fetch(`https://api.coingecko.com/api/v3/coins/bitcoin`).then((res) =>
+      res.json()
+    )
+  );
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  console.log(data)
+
+  return (
+    <div>
+      <h1>Cpoiny</h1>
+    </div>
+  );
+};
+
+export default EachCoin;
