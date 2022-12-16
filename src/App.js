1cf86c1269244c5b2016eb2c8c5d6892fa72c769
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 15:15:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,39 +7,40 @@ import EachCoin from "./pages/EachCoin";
 import { useSelector } from "react-redux";
 import Exchange from "./pages/Exchange";
 import CoinCategories from "./pages/CoinCategories";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
-  // const [coin, setCoin] = useState({});
-
-  // const eachCoinId = useSelector((state) => state.eachCoin);
-  // console.log(eachCoinId);
 
   // This might be a problem and needs to be changed
   // useQuery could be a good option
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-      )
-      .then((res) => {
-        setCoins(res.data);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
   // useEffect(() => {
-  //   axios.get(`https://api.coingecko.com/api/v3/coins/${eachCoinId}`)
-  //   .then(res => {
-  //     setCoin(res.data);
-  //     setLoading(false);
-  //   })
-  //   .catch(error => console.log(error))
+  //   axios
+  //     .get(
+  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  //     )
+  //     .then((res) => {
+  //       setCoins(res.data);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => console.log(error));
   // }, []);
+
+  const { data, error } = useQuery("repoData", () =>
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+    ).then((res) => {
+      res.json();
+      setLoading(false);
+    })
+  );
+
+  if (loading) return <LoadingSpinner />;
+
+  if (error) return "An error has occurred: " + error.message;
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -53,39 +54,32 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-    <div className="coin-app">
-      <Routes>
-        <Route
-          path="/"
-          exact
-          element={
-            !loading ? (
+      <div className="coin-app">
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
               <CoinTable
                 filteredCoins={filteredCoins}
                 handleChange={handleChange}
                 setLoading={setLoading}
               />
-            ) : (
-              <p>Loading...</p>
-            )
-          }
-        />
-        <Route
-          path="/exchange"
-          exact
-          element={<Exchange handleChange={handleChange} />}
-        />
-        <Route
-          path="/categories"
-          exact
-          element={<CoinCategories handleChange={handleChange} />}
-        />
-        <Route
-          path="/:ID"
-          element={!loading ? <EachCoin setLoading={setLoading} /> : <p>Loading...</p>}
-        />
-      </Routes>
-    </div>
+            }
+          />
+          <Route
+            path="/exchange"
+            exact
+            element={<Exchange handleChange={handleChange} />}
+          />
+          <Route
+            path="/categories"
+            exact
+            element={<CoinCategories handleChange={handleChange} />}
+          />
+          <Route path="/:ID" element={<EachCoin setLoading={setLoading} />} />
+        </Routes>
+      </div>
     </QueryClientProvider>
   );
 }
