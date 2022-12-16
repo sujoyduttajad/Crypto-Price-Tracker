18016bd452a3e4bc15cbadfb102097178fac78ef
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 15:58:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import EachCoin from "./pages/EachCoin";
 import { useSelector } from "react-redux";
 import Exchange from "./pages/Exchange";
 import CoinCategories from "./pages/CoinCategories";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
   //     .catch((error) => console.log(error));
   // }, []);
 
-  const { data, error } = useQuery("repoData", () =>
+  const { data, error } = useQuery("data", () =>
     fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
     ).then((res) => {
@@ -51,34 +51,30 @@ function App() {
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
 
-  const queryClient = new QueryClient();
-
   return (
     <Routes>
-      <QueryClientProvider client={queryClient}>
-        <Route
-          path="/"
-          exact
-          element={
-            <CoinTable
-              filteredCoins={filteredCoins}
-              handleChange={handleChange}
-              setLoading={setLoading}
-            />
-          }
-        />
-        <Route
-          path="/exchange"
-          exact
-          element={<Exchange handleChange={handleChange} />}
-        />
-        <Route
-          path="/categories"
-          exact
-          element={<CoinCategories handleChange={handleChange} />}
-        />
-        <Route path="/:ID" element={<EachCoin setLoading={setLoading} />} />
-      </QueryClientProvider>
+      <Route
+        path="/"
+        exact
+        element={
+          <CoinTable
+            filteredCoins={filteredCoins}
+            handleChange={handleChange}
+            setLoading={setLoading}
+          />
+        }
+      />
+      <Route
+        path="/exchange"
+        exact
+        element={<Exchange handleChange={handleChange} />}
+      />
+      <Route
+        path="/categories"
+        exact
+        element={<CoinCategories handleChange={handleChange} />}
+      />
+      <Route path="/:ID" element={<EachCoin setLoading={setLoading} />} />
     </Routes>
   );
 }
