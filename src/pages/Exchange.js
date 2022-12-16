4dd94350eadfeb5629a94a1b0c04581836186ede
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-16 16:40:45 +0530

    Update Exchange.js

--- src/pages/Exchange.js
+++ src/pages/Exchange.js
@@ -3,10 +3,10 @@ import Navbar from "../components/Navbar";
 
 const Exchange = ({ handleChange }) => {
   return (
-    <section className="coin-app">
+    <>
       <Navbar handleChange={handleChange} />
       <div>Exchange</div>
-    </section>
+    </>
   );
 };
 
