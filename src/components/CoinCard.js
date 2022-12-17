f9c2a857363b8b06fa827b4b0c5e64126639a8d6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-17 23:57:47 +0530

    Update CoinCard.js

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import { useStyles } from "../materialUi/GlobalStyles";
 
-const CoinCard = () => {
+
+const CoinCard = ({ data }) => {
+  const classes = useStyles();
   return (
-    <div>CoinCard</div>
-  )
-}
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {data.name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Market Cap - {data.market_cap_rank}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Community Score - {data.community_score}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Coingecko Score - {data.coingecko_score}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {data.description.en}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" size="small">Buy Now</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default CoinCard
+export default CoinCard;
