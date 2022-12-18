4cbc9703b52813915ff2bb7ad02c7203ac0acc6c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-18 18:28:55 +0530

    Update CoinCard.js

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -7,33 +7,35 @@ import {
   Button,
 } from "@mui/material";
 import { useStyles } from "../materialUi/GlobalStyles";
-
+import Tilt from "react-parallax-tilt";
 
 const CoinCard = ({ data }) => {
-  const classes = useStyles();
+  const classNamees = useStyles();
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {data.name}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Market Cap - {data.market_cap_rank}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Community Score - {data.community_score}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Coingecko Score - {data.coingecko_score}
-        </Typography>
-        <Typography variant="body2" component="p">
-          {data.description.en}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" size="small">Buy Now</Button>
-      </CardActions>
-    </Card>
+    <Tilt scale={0.95} transitionSpeed={300} gyroscope={true}>
+      <div className="card">
+        <Card classNameName={classNamees.card}>
+          <CardContent>
+            <div className="welcome">
+              <Typography variant="h5" component="h2">
+                {data.name}
+              </Typography>
+            </div>
+            <div className="year">
+              <Typography classNameName={classNamees.pos} color="textSecondary">
+                Market Cap - {data.market_cap_rank}
+              </Typography>
+              <Typography classNameName={classNamees.pos} color="textSecondary">
+                Community Score - {data.community_score}
+              </Typography>
+              <Typography classNameName={classNamees.pos} color="textSecondary">
+                Coingecko Score - {data.coingecko_score}
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Tilt>
   );
 };
 
