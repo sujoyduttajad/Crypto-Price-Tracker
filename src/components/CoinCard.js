c2ef33323a464359ee6d9ccb8f9bec39fa6de87b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-19 12:22:04 +0530

    Update CoinCard.js

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Card, CardContent, Typography } from "@mui/material";
-import { useStyles } from "../materialUi/GlobalStyles";
 import Tilt from "react-parallax-tilt";
 
 const CoinCard = ({ data }) => {
-  const classes = useStyles();
+ 
   return (
     <Tilt scale={0.95} transitionSpeed={300} gyroscope={true}>
       <div className="card">
@@ -15,11 +13,11 @@ const CoinCard = ({ data }) => {
         </div>
         <div className="year">
           <div className="coin-title">
-            <h4>{data.name}</h4>
+            <p>{data.name}</p>
           </div>
           <div className="market-info">
             <h2 >
-              {data.market_data.market_cap_change_24h}{" "}
+              ${data.market_data.current_price.usd}{" "}
               {data.symbol.toUpperCase()}
             </h2>
           </div>
