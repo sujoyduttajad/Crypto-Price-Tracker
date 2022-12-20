b809c9a5365a6af80eae42887caafa8f49b77c93
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-21 03:24:29 +0530

    Update Coin.js

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { useQuery } from "react-query";
-import CoinCard from "../components/CoinCard";
 import LoadingSpinner from "../components/LoadingSpinner";
 import Navbar from "../components/Navbar";
+
+const CoinCard = lazy(() => import("../components/CoinCard"));
 
 const EachCoin = ({ coinId }) => {
   // The data returned will be an object of the coin selected
   // and will contain all the relevant details
-  const { data, isLoading, error } = useQuery("repoData", () =>
+  const { data, isLoading, error } = useQuery("repoData", async () =>
     fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`).then((res) =>
       res.json()
     )
@@ -17,15 +18,15 @@ const EachCoin = ({ coinId }) => {
 
   if (error) return "An error has occurred: " + error.message;
 
-  // console.log(data);
+  console.log(data);
 
   return (
-    <>
+    <Suspense fallback={<LoadingSpinner />}>
       <Navbar coinId={coinId} />
       <section className="coin-info-container">
         <CoinCard data={data} />
       </section>
-    </>
+    </Suspense>
   );
 };
 
