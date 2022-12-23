2dd83e3846a9cb4305596c668eea14c1b5e098d0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-23 13:47:49 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const Info = () => {
+const Info = ({ data }) => {
   return (
-    <div className='info-container'>
-        <h3>General Info</h3>
-        <div className='info-row'>
-            
+    <div className="info-container">
+      <h3>General Info</h3>
+      <div className="info-row">
+        <p>Website</p>
+        <div className="chip">
+          <a href={data.links.homepage[0]}>
+            {data.links.homepage[0].replace(/[http://www.]/gi, "")}
+            </a>
         </div>
+      </div>
+      <div className="info-row">
+        <p>Website</p>
+        <div className="chip">
+          <a href={data.links.homepage[0]}>
+            {data.links.homepage[0].replace(/[http://www.]/gi, "")}
+            </a>
+        </div>
+      </div>
+      <div className="info-row">
+        <p>Website</p>
+        <div className="chip">
+          <a href={data.links.homepage[0]}>
+            {data.links.homepage[0].replace(/[http://www.]/gi, "")}
+            </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
