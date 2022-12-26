b39fc2c2050d0a9212d9a2694aec33f26a5bc0d1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-26 15:31:05 +0530

    Update MarketInfo.js

--- src/components/MarketInfo.js
+++ src/components/MarketInfo.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const MarketInfo = () => {
+const MarketInfo = ({ data }) => {
+  const regexSite = /^(https?:\/\/www.)/;
   return (
-    <div>MarketInfo</div>
-  )
-}
+    <div className="info-container">
+      <h3>Price Stats</h3>
+      <div className="chip">
+        <a href={data.links.homepage[0]} target="_blank" rel="noreferrer">
+          {data.links.homepage[0]?.replace(regexSite, "")}
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default MarketInfo
+export default MarketInfo;
