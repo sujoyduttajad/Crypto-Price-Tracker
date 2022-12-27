c94ae19854d49e77a87ccf3786344f65d1e925d2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-27 14:59:40 +0530

    Update MarketInfo.js

--- src/components/MarketInfo.js
+++ src/components/MarketInfo.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
 const MarketInfo = ({ data }) => {
   const regexSite = /^(https?:\/\/www.)/;
   return (
@@ -9,6 +14,12 @@ const MarketInfo = ({ data }) => {
         <a href={data.links.homepage[0]} target="_blank" rel="noreferrer">
           {data.links.homepage[0]?.replace(regexSite, "")}
         </a>
+      </div>
+      <div className="market-row">
+        <p>Market Cap</p>
+        <div className="chip">
+          <p>{formatter.format(data.market_data.market_cap.usd)}</p>
+        </div>
       </div>
     </div>
   );
