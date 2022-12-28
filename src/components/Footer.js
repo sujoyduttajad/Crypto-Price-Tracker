4856bceb9c76e13fcb6ed67308962a991f6a2d68
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-28 16:43:44 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../images/coinize2.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <NavLink to="/">
+            <div
+              className="coin-image-container"
+              style={{ userSelect: "none" }}
+            >
+              <img className="coin-logo" src={Logo} alt="logo" />
+            </div>
+          </NavLink>
 
-export default Footer
+          <p></p>
+          <div></div>
+        </Grid>
+        <Grid item xs={2}>
+          <ul>
+            <li>Market</li>
+            <li>Exchange</li>
+            <li>Categories</li>
+          </ul>
+        </Grid>
+        <Grid item xs={2}>
+          <ul>
+            <li>About Us</li>
+            <li>Blog</li>
+            <li>FAQ</li>
+          </ul>
+        </Grid>
+        <Grid item xs={2}>
+          <ul>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Reddit</li>
+          </ul>
+        </Grid>
+      </Grid>
+    </footer>
+  );
+};
+
+export default Footer;
