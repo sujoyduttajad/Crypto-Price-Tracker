4ad9112954e102fc9e65fdd8cd8300a51cf797cc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-28 22:01:14 +0530

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -61,14 +61,12 @@ export const cryptoData = [
     para: `The amount of coins that are circulating in the market and are tradeable by the public. It is comparable to looking at shares readily available in the market (not held & locked by insiders, governments).`,
   },
   {
-    para: `
-    The amount of coins that have already been created, minus any coins that have been burned (removed from circulation). It is comparable to outstanding shares in the stock market.
+    para: `The amount of coins that have already been created, minus any coins that have been burned (removed from circulation). It is comparable to outstanding shares in the stock market.
     
     Total Supply = Onchain supply - burned tokens`,
   },
   {
-    para: `
-    The maximum number of coins coded to exist in the lifetime of the cryptocurrency. It is comparable to the maximum number of issuable shares in the stock market.
+    para: `The maximum number of coins coded to exist in the lifetime of the cryptocurrency. It is comparable to the maximum number of issuable shares in the stock market.
     
     Max Supply = Theoretical maximum as coded`,
   },
