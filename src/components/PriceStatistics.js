5e498c0ea8fd9ce9355e6d0afcb4cfd93144b205
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-29 12:28:14 +0530

    Update PriceStatistics.js

--- src/components/PriceStatistics.js
+++ src/components/PriceStatistics.js
@@ -2,6 +2,8 @@ import React from "react";
 import { formatter } from "../utils/functions";
 
 const PriceStatistics = ({ data }) => {
+  const ratio =
+    data.market_data.total_volume.usd / data.market_data.market_cap.usd;
   return (
     <div className="info-container">
       <h3>USDT Price Statistics</h3>
@@ -15,8 +17,8 @@ const PriceStatistics = ({ data }) => {
         <p>24h Low / 24h High</p>
         <div className="chip">
           <p>
-            ${data.market_data.high_24h.usd.toFixed(4)} /{" "}
-            ${data.market_data.low_24h.usd.toFixed(4)}
+            ${data.market_data.high_24h.usd.toFixed(4)} / $
+            {data.market_data.low_24h.usd.toFixed(4)}
           </p>
         </div>
       </div>
@@ -24,9 +26,47 @@ const PriceStatistics = ({ data }) => {
         <p>7day Low / 7day High</p>
         <div className="chip">
           <p>
-            ${data.market_data.high_24h.usd.toFixed(4)} /{" "}
-            ${data.market_data.low_24h.usd.toFixed(4)}
+            ${data.market_data.high_24h.usd.toFixed(4)} / $
+            {data.market_data.low_24h.usd.toFixed(4)}
           </p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>Trading Volume</p>
+        <div className="chip">
+          <p>{formatter.format(data.market_data.total_volume.usd)}</p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>Market Cap Rank</p>
+        <div className="chip">
+          <p>#{data.market_data.market_cap_rank}</p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>Market Cap</p>
+        <div className="chip">
+          <p>{formatter.format(data.market_data.market_cap.usd)}</p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>Volume / Market Cap</p>
+        <div className="chip">
+          <p>{ratio.toFixed(4)}</p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>All-Time High</p>
+        <div className="chip">
+          <p>{formatter.format(data.market_data.ath.usd)}</p>{" "} 
+          <p>{data.market_data.ath_change_percentage.usd}</p>
+        </div>
+      </div>
+      <div className="market-row">
+        <p>All-Time Low</p>
+        <div className="chip">
+          <p>{formatter.format(data.market_data.atl.usd)}</p>
+          <p>{data.market_data.atl_change_percentage.usd}</p>
         </div>
       </div>
     </div>
