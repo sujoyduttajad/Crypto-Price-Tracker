24a966af9cff1fd7d3da11d26f968f73ff9767cd
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-12-31 21:01:04 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import banner from "../images/home-banner.jpg";
+
+const Home = ({ handleChange }) => {
+  return (
+    <>
+      <Navbar handleChange={handleChange} />
+      <section className="home-banner">
+        <div className="image-container">
+          <img src={banner} alt="banner" />
+        </div>
+        <div className="home-typograph">
+        <h1>Keep track of your favorite Crypto Coins</h1>
+        <p>Here you can learn, and track your favourite Cryptocurrencies</p>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
