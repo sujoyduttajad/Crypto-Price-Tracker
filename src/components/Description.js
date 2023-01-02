8f97d8cb9528dc6f64153f8b286163ca08f8b03f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-02 14:46:58 +0530

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,9 +4,9 @@ const Description = ({ data }) => {
   const text = data.description.en;
   return (
     <div className="market-container">
-      <h2 className="desc-header">
+      <h3 className="desc-header">
         Learn about <span>{data.name}</span>
-      </h2>
+      </h3>
       <div dangerouslySetInnerHTML={{ __html: text }} />
     </div>
   );
