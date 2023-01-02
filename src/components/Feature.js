f376755933faf5aea3176a0007a9030926abaa6e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-02 17:42:14 +0530

    Create Feature.js

---
+++ src/components/Feature.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+        <div aria-label='left-content'>
+            <h3>Our advantages, which you can enjoy</h3>
+            <p>Coinize will always provide you the best quality service platform for your convinience</p>
+        </div>
+        <div aria-label='right-content'>
+            <div>
+                <div><img /></div>
+                <div>
+                    <h4></h4>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Feature
