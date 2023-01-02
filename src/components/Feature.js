ac1963114b21b97344df533d9376ea007218fbc2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-02 22:50:22 +0530

    Update Feature.js

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Feature = () => {
   return (
-    <div>
-        <div aria-label='left-content'>
+    <div className='feature'>
+        <div className='left-container' aria-label='left-content'>
             <h3>Our advantages, which you can enjoy</h3>
             <p>Coinize will always provide you the best quality service platform for your convinience</p>
         </div>
-        <div aria-label='right-content'>
+        <div className='right-container' aria-label='right-content'>
             <div>
                 <div><img /></div>
                 <div>
