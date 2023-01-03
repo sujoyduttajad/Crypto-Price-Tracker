da7900da1952e796d6b1ceebe2dc69966b0fc2ae
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-03 16:09:41 +0530

    Update Feature.js

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
+import { features } from "../utils/data";
 
 const Feature = () => {
   return (
-    <div className='feature'>
-        <div className='left-container' aria-label='left-content'>
-            <h3>Our advantages, which you can enjoy</h3>
-            <p>Coinize will always provide you the best quality service platform for your convinience</p>
-        </div>
-        <div className='right-container' aria-label='right-content'>
-            <div>
-                <div><img /></div>
-                <div>
-                    <h4></h4>
-                    <p></p>
-                </div>
+    <div className="feature">
+      <div className="left-container" aria-label="left-content">
+        <h3>Our advantages, which you can enjoy</h3>
+        <p>
+          Coinize will always provide you the best quality service platform for
+          your convinience
+        </p>
+      </div>
+      <div className="right-container" aria-label="right-content">
+        {features.map((item) => (
+          <div key={item.id} className="grid-container">
+            <div className="image-box">
+              <img src={item.imgSrc} alt={item.heading} />
             </div>
-        </div>
+            <div className="content-box">
+              <h4>{item.heading}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
