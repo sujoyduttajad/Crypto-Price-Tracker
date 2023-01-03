ee64dbc66ce584f56c3dc515c9d9377b9a970b1c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-04 00:06:29 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,34 +8,43 @@ const Footer = () => {
     <footer>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <NavLink to="/">
-            <div
-              className="coin-image-container"
-              style={{ userSelect: "none" }}
-            >
-              <img className="coin-logo" src={Logo} alt="logo" />
-            </div>
-          </NavLink>
+          <div className="logo-container" style={{ userSelect: "none" }}>
+            <NavLink to="/">
+              <img src={Logo} alt="logo" />
+            </NavLink>
+          </div>
 
           <p></p>
           <div></div>
         </Grid>
         <Grid item xs={2}>
-          <ul> <h4>Explore</h4>
-          <NavLink className="styled-link" to="/"><li>Market</li></NavLink>
-          <NavLink className="styled-link" to="/exchange"><li>Exchange</li></NavLink>
-          <NavLink className="styled-link" to="/categories"><li>Categories</li></NavLink>
+          <ul>
+            {" "}
+            <h4>Explore</h4>
+            <NavLink className="styled-link" to="/">
+              <li>Market</li>
+            </NavLink>
+            <NavLink className="styled-link" to="/exchange">
+              <li>Exchange</li>
+            </NavLink>
+            <NavLink className="styled-link" to="/categories">
+              <li>Categories</li>
+            </NavLink>
           </ul>
         </Grid>
         <Grid item xs={2}>
-          <ul> <h4>About coinize</h4>
+          <ul>
+            {" "}
+            <h4>About coinize</h4>
             <li>About Us</li>
             <li>Blog</li>
             <li>FAQ</li>
           </ul>
         </Grid>
         <Grid item xs={2}>
-          <ul> <h4>Community</h4>
+          <ul>
+            {" "}
+            <h4>Community</h4>
             <li>Twitter</li>
             <li>Facebook</li>
             <li>Reddit</li>
