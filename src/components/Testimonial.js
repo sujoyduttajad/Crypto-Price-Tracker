98e5ec6a88634b8bcf4dedfa3ee6dfbfa54328f0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-13 14:07:37 +0530

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
-  )
-}
+    <section className="testimonial">
+      <div className="testimonial-header">
+        <h2>They can prove it themselves about us</h2>
+        <p>
+          Some of the people below have felt the quality of our platform, you
+          can give your testiminial below.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
