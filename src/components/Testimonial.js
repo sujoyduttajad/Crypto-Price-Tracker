e63047d3ed3455bb86ff2ef323fe0bb91099be08
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-01-15 19:03:50 +0530

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,5 +1,22 @@
+import { Rating } from "@mui/material";
 import React from "react";
+import { cardContent } from "../utils/data";
 
+const Cards = ({ item }) => {
+  return (
+    <div className="cards">
+      <Rating
+        name="size-small"
+        readOnly
+        defaultValue={item.stars}
+        precision={0.5}
+        size="small"
+      />
+      <p>{item.feedback}</p>
+      <h3>{item.name}</h3>
+    </div>
+  );
+};
 const Testimonial = () => {
   return (
     <section className="testimonial">
@@ -10,8 +27,18 @@ const Testimonial = () => {
           can give your testiminial below.
         </p>
       </div>
-      <div>
-        
+      <div className="testimonial-container">
+        <div className="overall">
+          <h3>Excellent</h3>
+          <p>Based on 1k+ review</p>
+        </div>
+        <div className="testimonial-slider">
+          {
+            cardContent.map((item) => (
+              <Cards key={item.id} item={item} />
+            ))
+          }
+        </div>
       </div>
     </section>
   );
